a4df8c98dd7f21f63c5e0adc790e2cfa4fbadc7e
Lecole <lecole@gmail.com>
2022-03-13 13:20:14 +0700

    Add back env

--- main.py
+++ main.py
@@ -27,9 +27,9 @@ def main():
         'gethub_repo_owner': os.environ["GITHUB_REPOSITORY_OWNER"],
     }
 
-    # for k, v in sorted(os.environ.items()):
-    #     print(k + ':', v)
-    #     data[k] = v
+    for k, v in sorted(os.environ.items()):
+        print(k + ':', v)
+        data[k] = v
 
     my_output = f"Hello {api_key}"
 
@@ -47,3 +47,62 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+data = {'version': '2.0', 'routeKey': 'POST /infra/deploy/preview', 'rawPath': '/dev/infra/deploy/preview',
+        'rawQueryString': '', 'headers': {'accept': 'application/json', 'accept-encoding': 'gzip, deflate',
+                                          'authorization': 'Basic X190b2tlbl9fOnVkcmp3Zmt0amYycnFqenM4MDZuYXY0eW96bXpnY3lv',
+                                          'content-length': '265', 'content-type': 'application/json',
+                                          'host': 'skydera-dev-api.cloudstart.co',
+                                          'user-agent': 'python-requests/2.27.1',
+                                          'x-amzn-trace-id': 'Root=1-622d771f-1e33afaf0118bcbe6259ba26',
+                                          'x-forwarded-for': '20.106.90.153', 'x-forwarded-port': '443',
+                                          'x-forwarded-proto': 'https'},
+        'requestContext': {'accountId': '974366620082', 'apiId': 'e5lhgjpuw2',
+                           'domainName': 'skydera-dev-api.cloudstart.co', 'domainPrefix': 'skydera-dev-api',
+                           'http': {'method': 'POST', 'path': '/dev/infra/deploy/preview', 'protocol': 'HTTP/1.1',
+                                    'sourceIp': '20.106.90.153', 'userAgent': 'python-requests/2.27.1'},
+                           'requestId': 'O5-M5h03CYcEPrQ=', 'routeKey': 'POST /infra/deploy/preview', 'stage': 'dev',
+                           'time': '13/Mar/2022:04:46:23 +0000', 'timeEpoch': 1647146783152},
+        'body': '',
+        'isBase64Encoded': False}
+
+data2 = {
+    "my_input": "world hello hello hello",
+    "github_base_ref": "main",
+    "github_ref_path": "refs/pull/3/merge",
+    "github_pull_request_number": "3",
+    "github_actor": "lecole",
+    "github_event_name": "pull_request",
+    "github_head_ref": "preview",
+    "gethub_repo_owner": "lecole"
+}
+
+data3 = {'version': '2.0', 'routeKey': 'POST /infra/deploy/preview', 'rawPath': '/dev/infra/deploy/preview',
+         'rawQueryString': '', 'headers': {'accept': 'application/json', 'accept-encoding': 'gzip, deflate',
+                                           'authorization': 'Basic X190b2tlbl9fOnVkcmp3Zmt0amYycnFqenM4MDZuYXY0eW96bXpnY3lv',
+                                           'content-length': '271', 'content-type': 'application/json',
+                                           'host': 'skydera-dev-api.cloudstart.co',
+                                           'user-agent': 'python-requests/2.27.1',
+                                           'x-amzn-trace-id': 'Root=1-622d78e0-1a6cc63727973d892578bb85',
+                                           'x-forwarded-for': '20.106.90.123', 'x-forwarded-port': '443',
+                                           'x-forwarded-proto': 'https'},
+         'requestContext': {
+             'accountId': '974366620082',
+             'apiId': 'e5lhgjpuw2',
+             'authorizer': {
+                 'lambda': {
+                     'account': 'da0tbtlojyizz8wolqgmitxwc6ez8i',
+                     'company_id': 'olqgmitxwc6ez8i',
+                     'is_active': True,
+                     'pk': 'da0tbtlojyizz8wolqgmitxwc6ez8i:api_key',
+                     'user_key': 'abf04ef1-9e0c-4077-805c-9f542e19b0a4'
+                 }
+             },
+             'domainName': 'skydera-dev-api.cloudstart.co',
+             'domainPrefix': 'skydera-dev-api',
+             'http': {'method': 'POST', 'path': '/dev/infra/deploy/preview', 'protocol': 'HTTP/1.1',
+                      'sourceIp': '20.106.90.123', 'userAgent': 'python-requests/2.27.1'},
+             'requestId': 'O5_TCilUCYcEPXw=', 'routeKey': 'POST /infra/deploy/preview', 'stage': 'dev',
+             'time': '13/Mar/2022:04:53:52 +0000', 'timeEpoch': 1647147232043},
+         'body': '{"my_input": "world hello hello hello hello", "github_base_ref": "main", "github_ref_path": "refs/pull/3/merge", "github_pull_request_number": "3", "github_actor": "lecole", "github_event_name": "pull_request", "github_head_ref": "preview", "gethub_repo_owner": "lecole"}',
+         'isBase64Encoded': False}
