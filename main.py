a5b722e6f2206c14724219907e37e6e1e59d187b
Lecole <lecole@gmail.com>
2022-03-15 10:11:15 +0700

    Update envs

--- main.py
+++ main.py
@@ -20,15 +20,14 @@ def main():
         'namespace': os.environ["INPUT_SKYNAMESPACE"],
         'git_intg_access_id': os.environ["INPUT_SKYGITINTGACCESSID"],
         'git_repo_slug': os.environ["INPUT_SKYGITREPOSLUG"],
+
         'gh_pr_number': os.environ["INPUT_PRNUMBER"],
-
-
-        'github_base_ref': os.environ["GITHUB_BASE_REF"],
-        'github_pull_request_number': os.environ["GITHUB_REF"].split('/')[2],
-        'github_actor': os.environ["GITHUB_ACTOR"],
-        'github_event_name': os.environ["GITHUB_EVENT_NAME"],
-        'github_head_ref': os.environ["GITHUB_HEAD_REF"],
-        'gethub_repo_owner': os.environ["GITHUB_REPOSITORY_OWNER"],
+        'gh_base_ref': os.environ["GITHUB_BASE_REF"],
+        # 'gh_pull_request_number': os.environ["GITHUB_REF"].split('/')[2],
+        'gh_actor': os.environ["GITHUB_ACTOR"],
+        'gh_event_name': os.environ["GITHUB_EVENT_NAME"],
+        'gh_head_ref': os.environ["GITHUB_HEAD_REF"],
+        'gh_repo_owner': os.environ["GITHUB_REPOSITORY_OWNER"],
     }
 
     query_data = requests.post(
